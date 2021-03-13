3edced28c92fed047a7c8d03a175cd3b1906a8ca
NashidC <nash.chowdhury@gmail.com>
2021-03-12 22:31:17 -0500

    landing page updated

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,19 @@
+import React from "react"
+import { Link } from "react-router-dom"
 
 function LandingPage() {
 
-    return (
-      <div>
-          <h1> LandingPage</h1>
+  return (
+    <div className="container">
+      <div className="home" >
+        <h1> LandingPage</h1>
+        <img src="https://i.imgur.com/mh2x7DI.png" />
+        <h2>Welcome to Cryptofy Hub</h2>
+        <Link to="/Aboutus"><button>Learn About Us</button></Link>
+        <Link to="/Home"><button>Take Me To Home</button></Link>
       </div>
-    )
-  }
-  
-  export default LandingPage;
+    </div>
+  )
+}
+
+export default LandingPage;
