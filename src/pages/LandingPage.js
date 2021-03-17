f907f8f729fce7623768d53ac4c8b1ec4448096d
Shnaeem <62766356+Shnaeem@users.noreply.github.com>
2021-03-17 18:30:35 -0400

    trendingcoins

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,14 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function LandingPage() {
+
 
     return (
       <div>
           <h1> LandingPage</h1>
+            
+
       </div>
     )
   }
