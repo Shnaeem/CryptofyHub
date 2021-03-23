f12cc0d0c4ecaa89324088c2153871c4711acefb
NashidC <nash.chowdhury@gmail.com>
2021-03-23 14:37:54 -0400

    landing page style completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,30 +5,32 @@ function Home() {
 
   const [data, setData] = useState([]);
 
-  const getTrendingData = async() =>{
-    const output = await axios.get ("https://api.coingecko.com/api/v3/search/trending");
+  const getTrendingData = async () => {
+    const output = await axios.get("https://api.coingecko.com/api/v3/search/trending");
     console.log(output)
     setData(output.data.coins)
   };
 
-  useEffect( ()=>{
+  useEffect(() => {
     getTrendingData();
-}, []);
+  }, []);
 
-    return (
-      <div>
-          <h1> Top 7 Trending Currencies:</h1>
-          {data.map((coins, index) => {
-              return (
-               <div key={index}>
-                  <h2>{coins.item.name} </h2> 
-                  <h3> Market Cap Rank: {coins.item.market_cap_rank} </h3>
-                  <h3> Symbol: {coins.item.symbol} </h3>
-              </div>
-              ) 
-            })}
-      </div>
-    )
-  }
-  
-  export default Home;
+  return (
+    <div className="trend-text">
+      <h1> Top 7 Trending Currencies:</h1>
+      {data.map((coins, index) => {
+        return (
+          <div >
+            <div key={index}>
+              <h2>{coins.item.name} </h2>
+              <h3> Market Cap Rank: {coins.item.market_cap_rank} </h3>
+              <h3> Symbol: {coins.item.symbol} </h3>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Home;
